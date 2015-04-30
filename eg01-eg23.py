985924d71a1da6bd027f2d25f727cda999a7439a
deniadjokic <denia.djokic@gmail.com>
2015-04-30 19:31:22 +0000

    extended years

--- eg01-eg23.py
+++ eg01-eg23.py
@@ -1,5 +1,6 @@
 """Generates an input file for transitioning from EG01 -> EG23"""
 from __future__ import unicode_literals, print_function
+
 from math import ceil
 
 try:
@@ -95,8 +96,8 @@ BASE_SIM = {"simulation": {
         {"name": "FR Fuel", "solution_priority": 1.0}, 
         {"name": "WASTE", "solution_priority": 1.0}
         ], 
-    "control": {"duration": 12 * 251, "startmonth": 1, "startyear": 1964}, 
-    #"control": {"duration": 12 * 20, "startmonth": 1, "startyear": 2005}, 
+    #"control": {"duration": 12 * 251, "startmonth": 1, "startyear": 1964}, 
+    "control": {"duration": 12 * 151, "startmonth": 1, "startyear": 1964}, 
     "facility": [
         {"name": "FR Fuel Fab",
          "config": {"FuelfabFacility": {
